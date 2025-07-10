885520ba131eef49bacff1c16c62403fe546e115
MinhDuyen494 <ngthminhduyen04@gmail.com>
2025-07-10 20:06:42 +0700

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 app.enableCors({
-  origin: 'http://localhost:3001', // Cho phép từ frontend Next.js
+  origin: 'http://localhost:5173', // Cho phép từ frontend
   credentials: true,               // (Nếu có dùng cookie/session)
 });
 app.useGlobalPipes(new ValidationPipe({
